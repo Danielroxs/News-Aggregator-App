75ba3b710f1d7488b5d1115b561677268b59ea35
Danielroxs <danroxsll@gmail.com>
2024-05-01 17:32:12 -0600

    News bugg fixed

--- script.js
+++ script.js
@@ -35,15 +35,59 @@ function fetchNews(isSearching){
 
         lastArticleCount = articlesWithImage.length
 
-        articlesWithImage.foreach(article => {
+        articlesWithImage.forEach(article => {
             const newsItem = `
                 <div class="newsItem">
                     <div class="newsImage">
                         <img src="${article.urlToImage}"
                         alt="${article.title}">
-                    
-            `
-        })
+                    </div>
+                    <div class="newsContent">
+                        <div class="info">
+                            <h5>${article.source.name}</h5>
+                            <span>|</span>
+                            <h5>${article.publishedAt}</h5>
+                        </div>
+                        <h2>${article.title}</h2>
+                        <p>${article.description}</p>
+                        <a href="${article.url}" target="_blank">Read More</a>
+                    </div>
+                </div>
+            `;
+            newsContainer.innerHTML += newsItem;
+        });
 
-    })
+        currentPage++;
+        isLoading = false;
+    }).catch(error => {
+        console.error("There was an error fetching the news :", error);
+        isLoading = false;
+    });
 }
+
+function displayNoMoreNews() {
+    const newsContainer = document.getElementById('newsContainer');
+    newsContainer.innerHTML += '<p>No more news to load.</p>';
+}
+
+window.onscroll = function () {
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight -10) {
+        if(currentKeyword) {
+            fetchNews(true);
+        } else {
+            fetchNews(false);
+        }
+    }
+}
+
+document.getElementById('searchKeyword').addEventListener('input', function () {
+    currentPage = 1;
+    currentCategory = null;
+    currentKeyword = this.value;
+});
+
+document.getElementById('fetchCategory').addEventListener('click', function () {
+    currentPage = 1;
+    currentKeyword = null;
+    fetchNews(false);
+});
